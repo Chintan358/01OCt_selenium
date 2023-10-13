bcf484a305a5e80bc042a9487e43cc5115bbe4e8
chintan <chintan.tops@gmail.com>
2023-10-13 18:53:08 +0530

    collection

--- Learning_corejava/src/fileio/F002_FileInputStream.java
+++ Learning_corejava/src/fileio/F002_FileInputStream.java
@@ -3,16 +3,19 @@ package fileio;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Scanner;
 
 public class F002_FileInputStream {
 	public static void main(String[] args) {
 		
-		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter name : ");
+		String name = sc.next();
 		
 		FileInputStream fis = null;
 		
 		try {
-			fis = new FileInputStream("C:\\Chintan_work\\Files\\data.txt");
+			fis = new FileInputStream("C:\\Chintan_work\\Files\\"+name+".txt");
 			int i =  fis.read();
 			while(i !=-1)
 			{
