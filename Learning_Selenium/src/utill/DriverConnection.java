238ed3eacd8bc57aedee6efea73767de6f4a0bb9
chintan <chintan.tops@gmail.com>
2023-11-06 19:20:55 +0530

    practice

--- Learning_Selenium/src/utill/DriverConnection.java
+++ Learning_Selenium/src/utill/DriverConnection.java
@@ -13,7 +13,7 @@ public class DriverConnection {
 		WebDriver driver = new ChromeDriver();
 		driver.manage().window().maximize();
 		driver.get(url);
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		return driver;
 	}
 }
